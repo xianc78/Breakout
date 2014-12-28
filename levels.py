5b9351394285bb421a1b346f6dcecf98b785505a
Nathan Yong <xianc78@gmail.com>
2014-12-27 20:10:09 -0600

    Added third level

--- levels.py
+++ levels.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 import constants
 pygame.init()
 from brick import Brick
@@ -8,9 +8,12 @@ class level:
 	brick_list = []
 
 	def createLevel(self):
+		# Set x and y coordinates to 0
 		x = y = 0
 		for line in self.layout.split():
+			# Go through each line
 			for brick in line:
+				# Go through each character in each line and create a color brick or a space according to the character
                                 if brick == "w":
                                         self.brick_list.append(Brick(x, y, constants.WHITE))
                                 elif brick == "b":
@@ -19,8 +22,13 @@ class level:
                                         self.brick_list.append(Brick(x, y, constants.GREEN))
                                 elif brick == "r":
                                         self.brick_list.append(Brick(x, y, constants.RED))
-                                else:
+				elif brick == "_":
                                         pass
+				else:
+					print brick + " is not a valid character"
+					raw_input("<press enter>")
+					pygame.quit
+					sys.exit()
                                 x += 38 + constants.BRICK_MARGIN
 			x = 0
 			y += 13 + constants.BRICK_MARGIN
@@ -28,20 +36,30 @@ class level:
 class level1(level):
 	def __init__(self):
 		self.layout = '''
-		wwwwwwwww
-		bbbbbbbbb
-		ggggggggg
-		rrrrrrrrr
+		wwwwwwwwwwwwwwwwwww
+		bbbbbbbbbbbbbbbbbbb
+		ggggggggggggggggggg
+		rrrrrrrrrrrrrrrrrrr
 		'''
 		self.createLevel()
 
 class level2(level):
 	def __init__(self):
 		self.layout = '''
-		  wwwwww
-		 bbbbbbbb
-		 gggggggg
-		 rrrrrrrr
+		_______wwwww_______
+		__bbbbbbbbbbbbbbb__
+		_ggggggggggggggggg_
+		rrrrrrrrrrrrrrrrrrr
 		 '''
 		self.createLevel()
+
+class level3(level):
+        def __init__(self):
+                self.layout = '''
+                ________ww_________
+                ____b_____b________
+                _____g____g_gg_____
+                __rrr--rr--rr______
+                '''
+                self.createLevel()
 
